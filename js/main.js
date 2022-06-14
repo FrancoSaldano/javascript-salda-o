6776270beaae56f7d8032bbd6c96a702b7db6e15
Franco Saldaño <francosaldano1@gmail.com>
2022-06-14 20:46:57 -0300

    beta v2.0

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-  class Register {
+class Register {
     constructor(nickdata, passdata) {
         this.datauser = []
         this.datauser.push(nickdata)
@@ -10,7 +10,7 @@
 }
 
 class Login {
-    constructor(tryNick, tryPass,fn) {
+    constructor(tryNick, tryPass, fn) {
         this.datalogin = []
         this.user = fn
         this.datalogin.push(tryNick)
@@ -49,7 +49,7 @@ class Login {
         let count = 0
         let tryPassfunc
         let tryNickfunc
-        let result=[]
+        let result = []
         alert("Solamente tiene 3 intentos")
         for (let i = 0; (i <= 2) && (tryPassfunc != this.user[1]) && (tryNickfunc != this.user[0]); i++) {
             count += 1
@@ -58,22 +58,22 @@ class Login {
                 tryNickfunc = prompt("Ingrese el nick:  " + this.user[0]).toString()
                 tryPassfunc = prompt("Ingrese la contraseña: " + this.user[1]).toString()
             }
-        } result.push(tryNickfunc),result.push(tryPassfunc)
+        } result.push(tryNickfunc), result.push(tryPassfunc)
         return result
     }
-    compareArray (array1, array2) {
+    compareArray(array1, array2) {
         var i = array1.length;
         if (i != array2.length) return false;
 
         while (i--) {
-          if (array1[i] !== array2[i]) return false;
+            if (array1[i] !== array2[i]) return false;
         }
         return true;
-      }
+    }
     login() {
         if (this.datalogin[0] == this.user[0] && this.datalogin[1] == this.user[1]) {
             this.alertSucces()
-        } else if (this.compareArray(this.attemptsPassword(),this.user)) {
+        } else if (this.compareArray(this.attemptsPassword(), this.user)) {
             this.alertSucces()
         } else {
             this.alertError()
@@ -81,6 +81,14 @@ class Login {
     }
 }
 
-var registrar= new Register(prompt("registra este nick: "),prompt("registra esta contraseña: "))
+var registrar = new Register(prompt("registra este nick: "), prompt("registra esta contraseña: "))
 var logeo = new Login(prompt("nick: "), prompt("contraseña: "), registrar.enviar())
 logeo.login()
+
+//////
+
+let userName = document.createElement('h2')
+userName.innerHTML = `Bienvenido! ${registrar.enviar()[0]}`
+let containerEspecial = document.querySelector('div.container--especial')
+containerEspecial.append(userName)
+userName.classList.add('p-5')
